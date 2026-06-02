7bf1ac4efea953c340e388c51571bde4e85f6726
Abreham Yeshitla <abrhamyeshitla990@gmail.com>
2026-06-02 20:45:30 +0300

    test

--- TmsCore/obj/Debug/net10.0/TmsCore.AssemblyInfo.cs
+++ TmsCore/obj/Debug/net10.0/TmsCore.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TmsCore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa415ed9f5064c2d30eb4939b6542face2e1b8fa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3e24a858aba8dd1ab310182f1f5c649dd72f72bb")]
 [assembly: System.Reflection.AssemblyProductAttribute("TmsCore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TmsCore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
